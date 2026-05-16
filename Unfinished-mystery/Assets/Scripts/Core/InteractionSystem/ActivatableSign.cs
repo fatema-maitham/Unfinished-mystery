2c7df0f02f3656e70fa2c771029fbedfc0031026
Fatima <202304056@student.polytechnic.bh>
2026-05-17 01:50:04 +0300

    activatable sign script

--- Unfinished-mystery/Assets/Scripts/Core/InteractionSystem/ActivatableSign.cs
+++ Unfinished-mystery/Assets/Scripts/Core/InteractionSystem/ActivatableSign.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
 
-public class ActivatableSign : MonoBehaviour
+public class ActivatableSign : MonoBehaviour, IActivatable
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private string label    = "Read";
+    [SerializeField] private string subLabel = "";
+    [TextArea(3, 8)]
+    [SerializeField] private string signText = "Enter your text here.";
+
+    public string ActivationLabel => label;
+    public string ActivationHint  => subLabel;
+    public bool   CanActivate     => true;
+
+    public void OnActivate(GameObject source)
     {
-        
+        ActivationDialogUI.ShowText(signText);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void OnActivatableFocus()  { }
+    public void OnActivatableBlur()   { }
 }
