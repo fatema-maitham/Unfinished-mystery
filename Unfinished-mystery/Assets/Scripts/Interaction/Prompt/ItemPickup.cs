eff972abca64e97dc29793d306e9a3c190689b6d
maramshubbar <maram20052@gmail.com>
2026-05-20 21:17:33 +0300

    add interaction prompt support for item pickup

---
+++ Unfinished-mystery/Assets/Scripts/Interaction/Prompt/ItemPickup.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+using InventoryFramework;
+
+public class ItemPickup : MonoBehaviour
+{
+    [Header("Prompt")]
+    [SerializeField] private InteractionPromptUI promptUI;
+    [SerializeField] private string promptText = "Pick up item";
+
+    [Header("Inventory Item")]
+    [SerializeField] private Item item;
+    [SerializeField] private int amount = 1;
+
+    [Header("Pickup Settings")]
+    [SerializeField] private bool destroyAfterPickup = true;
+
+    private bool playerInRange = false;
+    private ItemPickupHandler pickupHandler;
+
+    private void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            PickUpItem();
+        }
+    }
+
+    private void PickUpItem()
+    {
+        if (pickupHandler == null)
+        {
+            Debug.LogError("ItemPickupHandler not found on Player!");
+            return;
+        }
+
+        if (item == null)
+        {
+            Debug.LogError("Item is not assigned on " + gameObject.name);
+            return;
+        }
+
+        pickupHandler.PickupItem(item, amount);
+
+        if (promptUI != null)
+        {
+            promptUI.HidePrompt();
+        }
+
+        Debug.Log(item.name + " picked up and added to inventory.");
+
+        if (destroyAfterPickup)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+
+        playerInRange = true;
+        pickupHandler = other.GetComponent<ItemPickupHandler>();
+
+        if (promptUI != null)
+        {
+            promptUI.ShowPrompt(promptText);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+
+        playerInRange = false;
+        pickupHandler = null;
+
+        if (promptUI != null)
+        {
+            promptUI.HidePrompt();
+        }
+    }
+}
