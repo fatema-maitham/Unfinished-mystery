b2ba14c88fd22183824486eac20d81be0439faed
Fatema Maitham <fatemamaitham1@gmail.com>
2026-05-27 21:03:05 +0300

    loop update working

--- Unfinished-mystery/AGENTS.md
+++ Unfinished-mystery/AGENTS.md
@@ -4,19 +4,11 @@
 - Active scene:
   - Name: Level2_Perfect
   - Tags:
-    - Untagged, Respawn, Finish, EditorOnly, MainCamera, Player, GameController, Keypad, Interactable, CinemachineTarget
-  - Layers:
-    - Default, TransparentFX, Ignore Raycast, Interactable, Water, UI, Interactable, Ground
-- Active game object:
-  - Name: GameManager (1)
-  - Tag: Untagged
-  - Layer: Default
-  - Name: L2BadgePopup
     - Untagged, Respawn, Finish, EditorOnly, MainCamera, Player, GameController, Keypad, Interactable, CinemachineTarget, Door
   - Layers:
     - Default, TransparentFX, Ignore Raycast, Interactable, Water, UI, Interactable, Ground
 - Active game object:
-  - Name: HUD_Canvas(DrawerOpen)
+  - Name: book_1 (1)
   - Tag: Untagged
-  - Layer: UI
+  - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Unfinished-mystery/Assets/Scripts/LoopSystem/LoopChangeSystem.cs
+++ Unfinished-mystery/Assets/Scripts/LoopSystem/LoopChangeSystem.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LoopChangeSystem : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class LoopChangeSystem : MonoBehaviour
     {
         public string objectName;
         public GameObject target;
+
+        [Header("Use Local Transform")]
+        public bool useLocalPosition = true;
 
         [Header("Position for Each Loop")]
         public Vector3[] loopPositions = new Vector3[5];
@@ -23,12 +27,17 @@ public class LoopChangeSystem : MonoBehaviour
     public int currentLoop = 1;
     public int maxLoops = 5;
 
+    [Header("UI Text")]
+    public TMP_Text loopText;
+
     [Header("Objects That Change Each Loop")]
     public List<LoopObject> loopObjects = new List<LoopObject>();
 
     private void Start()
     {
+        currentLoop = Mathf.Clamp(currentLoop, 1, maxLoops);
         ApplyLoopChanges();
+        UpdateLoopUI();
     }
 
     public void GoToNextLoop()
@@ -39,12 +48,15 @@ public class LoopChangeSystem : MonoBehaviour
             currentLoop = maxLoops;
 
         ApplyLoopChanges();
+        UpdateLoopUI();
     }
 
     public void SetLoop(int loopNumber)
     {
         currentLoop = Mathf.Clamp(loopNumber, 1, maxLoops);
+
         ApplyLoopChanges();
+        UpdateLoopUI();
     }
 
     private void ApplyLoopChanges()
@@ -53,17 +65,37 @@ public class LoopChangeSystem : MonoBehaviour
 
         foreach (LoopObject item in loopObjects)
         {
-            if (item.target == null)
+            if (item == null || item.target == null)
                 continue;
 
+            if (item.loopActiveStates != null && item.loopActiveStates.Length > loopIndex)
+            {
+                item.target.SetActive(item.loopActiveStates[loopIndex]);
+            }
+
             if (item.loopPositions != null && item.loopPositions.Length > loopIndex)
-                item.target.transform.position = item.loopPositions[loopIndex];
+            {
+                if (item.useLocalPosition)
+                    item.target.transform.localPosition = item.loopPositions[loopIndex];
+                else
+                    item.target.transform.position = item.loopPositions[loopIndex];
+            }
 
             if (item.loopRotations != null && item.loopRotations.Length > loopIndex)
-                item.target.transform.rotation = Quaternion.Euler(item.loopRotations[loopIndex]);
+            {
+                if (item.useLocalPosition)
+                    item.target.transform.localRotation = Quaternion.Euler(item.loopRotations[loopIndex]);
+                else
+                    item.target.transform.rotation = Quaternion.Euler(item.loopRotations[loopIndex]);
+            }
+        }
+    }
 
-            if (item.loopActiveStates != null && item.loopActiveStates.Length > loopIndex)
-                item.target.SetActive(item.loopActiveStates[loopIndex]);
+    private void UpdateLoopUI()
+    {
+        if (loopText != null)
+        {
+            loopText.text = "Loop " + currentLoop + " / " + maxLoops;
         }
     }
 }
